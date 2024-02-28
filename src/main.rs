576016dd2ec79503e503725daa1d1f8e7548bc27
gil7788 <gil7788@gmail.com>
2024-02-28 20:07:02 +0200

    Remove Random Number Generation Functionality

--- src/main.rs
+++ src/main.rs
@@ -9,47 +9,9 @@ use serde_json::{Map};
 use to_do::ItemTypes;
 use to_do::to_do_factory;
 
-use rand::prelude::*;
 use std::env;
 use crate::processes::process_input;
 
-/// Generates a random float number between 0 and 10.
-///
-/// # Arguments
-/// * `generator` (&mut ThreadRng): The random number generator.
-///
-/// # Returns
-/// A random number between 0 and 10 as f64.
-fn generate_float(generator: &mut ThreadRng) -> f64 {
-    let r: f64 = generator.gen();
-    r * 10.0
-}
-
-/// Trait defining basic user behavior.
-trait IsUser {
-    /// Checks if the struct instance is a user.
-    ///
-    /// # Returns
-    /// Always returns true for user instances.
-    fn is_user() -> bool {
-        true
-    }
-}
-
-/// Represents a user with a name and age.
-///
-/// # Attributes
-/// * `name` (String): The name of the user.
-/// * `age` (i8): The age of the user.
-#[allow(dead_code)]
-struct User {
-    name: String,
-    age: i8,
-}
-
-/// Checks the environment the binary is running in.
-///
-/// It identifies if the running environment is development, production, or neither.
 fn check_binary_environment() {
     let args: Vec<String> = env::args().collect();
     let path: &str = &args[0];
@@ -62,21 +24,8 @@ fn check_binary_environment() {
     }
 }
 
-fn generate_number() {
-    // Generate and print a random number
-    let mut rng: ThreadRng = rand::thread_rng();
-    let r: f64 = generate_float(&mut rng);
-    println!("Random number: {}", r);
-
-    // Print command-line arguments
-    let args: Vec<String> = env::args().collect();
-    println!("Args: {:?}", args);
-}
-
 fn main() {
     check_binary_environment();
-    println!("Hello, world!");
-    generate_number();
 
     let to_do: ItemTypes = to_do_factory("pending", "make").unwrap();
     match to_do {
